f91d0ee9e9b8c5a354d6e3d0563f71d7bf5f47c2
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-29 09:57:38 -0800

    initialized store

--- scripts/store.js
+++ scripts/store.js
@@ -1,1 +1,17 @@
 'use strict';
+const store = (function(){
+    
+    const addBookmark = function(item){
+        this.items.push(item);
+    };
+    
+    const deleteBookmark = function(item){
+
+    };
+    
+    return {
+        items: [],
+        addBookmark
+    };
+
+}());
